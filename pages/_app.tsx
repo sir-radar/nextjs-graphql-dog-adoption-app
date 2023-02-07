0e21d30a35a21de88d334b5c6f700a32aa7220b7
Samson Nwokike <nwokikesamson@gmail.com>
2023-02-07 04:36:43 +0100

    home ui and data display

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,20 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import { Hydrate, QueryClientProvider } from 'react-query';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import { queryClient } from '../src/api';
+import Layout from '../components/Layout';
+
+import '../styles/globals.css';
+
+function App({ Component, pageProps }) {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Hydrate state={pageProps.dehydratedState}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </Hydrate>
+    </QueryClientProvider>
+  );
 }
+
+export default App;
