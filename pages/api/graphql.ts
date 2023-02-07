b4828238c7698a7ac478e411f6f75f046e2253d3
Samson Nwokike <nwokikesamson@gmail.com>
2023-02-07 03:57:51 +0100

    refactor graphql code

--- pages/api/graphql.ts
+++ pages/api/graphql.ts
@@ -1,32 +1,11 @@
 import 'reflect-metadata';
 import { ApolloServer } from 'apollo-server-micro';
 import type { NextApiRequest, NextApiResponse } from 'next';
-import {
-  buildSchema,
-  Resolver,
-  Query,
-  Arg,
-  ObjectType,
-  Field,
-  ID,
-} from 'type-graphql';
-
-@ObjectType()
-export class Dog {
-  @Field(() => ID)
-  name!: string;
-}
-
-@Resolver(Dog)
-export class DogResolver {
-  @Query(() => [Dog])
-  dogs(): Dog[] {
-    return [{ name: 'Bo' }, { name: 'Lassie' }];
-  }
-}
+import { buildSchema } from 'type-graphql';
+import { DogsResolver } from '../../src/schema/dogs.resolver';
 
 const schema = await buildSchema({
-  resolvers: [DogResolver],
+  resolvers: [DogsResolver],
 });
 
 const server = new ApolloServer({ schema });
