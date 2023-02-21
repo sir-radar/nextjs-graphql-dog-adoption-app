c94d7a3a730fb3a49f2c795766413bec4491d5cd
Samson Nwokike <nwokikesamson@gmail.com>
2023-02-21 09:53:17 +0100

    update graphql source

--- src/api.ts
+++ src/api.ts
@@ -3,7 +3,9 @@ import { QueryClient } from 'react-query';
 
 import { getSdk } from '../src/generated/graphql';
 
-const gqlClient = new GraphQLClient('http://localhost:3000/api/graphql');
+const gqlClient = new GraphQLClient(
+  'https://rad-cactus-fa0a13.netlify.app/api/graphql'
+);
 export const { getDogs, dogByName } = getSdk(gqlClient);
 
 export const queryClient = new QueryClient({
